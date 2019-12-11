7b777ebb25dad7508325724484b7fa2a5c929f0f
Ilya Kontaev <ideis@yandex.ru>
2019-12-11 16:13:05 +0300

    add data transforms

---
+++ data/transforms.py
@@ -0,0 +1,30 @@
+import torch
+from torchvision.transforms import *
+
+def transform_train(img):
+    transform = Compose([
+        ToTensor(),
+        Normalize(mean=[0.5] * 3, std=[0.5] * 3)
+    ])
+
+    img_tensor = transform(img)
+    return img_tensor
+
+def transform_val(img):
+    transform = Compose([
+        ToTensor(),
+        Normalize(mean=[0.5] * 3, std=[0.5] * 3)
+    ])
+
+    img_tensor = transform(img)
+    return img_tensor
+
+def transform_inv(img_tensor):
+    transform = Compose([
+        Normalize(mean=[-1.] * 3,
+                  std=[2.] * 3),
+        ToPILImage()
+    ])
+    img = transform(img_tensor)
+    return img
+
