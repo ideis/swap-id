98da4c6641deb9442fd6c756c814104e4267841b
Ilya Kontaev <ideis@yandex.ru>
2019-12-16 14:35:10 +0300

    change training hyperparameters

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import click
-import cv2
 import numpy as np
 
 import torch
@@ -12,17 +11,17 @@ from data.faces import LFW
 from data.faces import Celebs
 from data.transforms import transform_train, transform_val
 from trainer import Trainer 
-from discriminator_trainer import DiscriminatorTrainer
 
 @click.command()
 @click.option('--model_dir', default='models/default')
 @click.option('--epochs', default=100)
-@click.option('--batch_size', default=128)
-@click.option('--lr', default=0.01)
-@click.option('--eval_every', default=50)
-@click.option('--generate_every', default=100)
+@click.option('--batch_size', default=256)
+@click.option('--lr', default=0.05)
+@click.option('--eval_every', default=200)
+@click.option('--generate_every', default=500)
+@click.option('--save_every', default=5000)
 @click.option('--num_workers', default=4)
-def main(model_dir, epochs, batch_size, lr, eval_every, generate_every, num_workers):
+def main(model_dir, epochs, batch_size, lr, eval_every, generate_every, save_every, num_workers):
 
     datasets = {'train': Celebs(db_path='datasets/train_db', transform=transform_train),
                 'val': LFW(path='datasets/lfw.bin', transform=transform_val)}
@@ -38,21 +37,16 @@ def main(model_dir, epochs, batch_size, lr, eval_every, generate_every, num_work
                                      shuffle=False,
                                      num_workers=num_workers)}
 
-    trainer_generator = Trainer(model_dir='mobiface_generator_mse',
-                                g_optimizer=FusedAdam,
+    traine = Trainer(model_dir='mobiface_generator_mse',
+                                g_optimizer=FusedSGD,
                                 d_optimizer=FusedSGD,
                                 lr=lr,
                                 num_classes=datasets['train'].num_classes)
     
-    # trainer_discriminator = DiscriminatorTrainer(model_dir='checkpoints/mobiface_airface_ce',
-    #                                optimizer=FusedSGD,
-    #                                lr=lr,
-    #                                num_classes=datasets['train'].num_classes)
-                
-    # trainer_generator.load_discriminator('checkpoints/mobiface_airface_ce', load_last=True)
+    # trainer.load_discriminator('checkpoints/mobiface_airface_ce', load_last=True)
 
     for epoch in range(1, epochs + 1):
-        trainer_generator.train_loop(dataloaders, eval_every, generate_every)
+        trainer.train_loop(dataloaders, eval_every, generate_every, save_every)
 
 if __name__ == "__main__":
     main()
