e057f4ccd8d9012b6363e5eeecb29f135431586e
Ilya Kontaev <ideis@yandex.ru>
2019-12-16 14:37:25 +0300

    refactor loss names. add identification evaluation

--- trainer.py
+++ trainer.py
@@ -4,6 +4,8 @@ import random
 from math import cos, pi
 from tqdm import tqdm
 from PIL import Image
+import numpy as np
+import sklearn.metrics as metrics
 
 import torch
 import torch.nn as nn
@@ -36,17 +38,18 @@ class Trainer(nn.Module):
         self.mae_criterion = nn.SmoothL1Loss()
         self.bce_criterion = nn.BCEWithLogitsLoss()
         self.ce_criterion = nn.CrossEntropyLoss()
-        self.mae_weight = 1
-        self.bce_weight = 1
-        self.ce_weight = 0.01
-        self.bce_real_weight = 1
-        self.bce_fake_weight = 1
+        self.generator_mae_weight = 0
+        self.generator_bce_weight = 0.0001
+        self.generator_ce_weight = 1
+        self.discriminator_bce_real_weight = 0.0001
+        self.discriminator_bce_fake_weight = 0.0001
+        self.discriminator_ce_weight = 1
 
         self.lr = lr
-        self.g_optimizer = g_optimizer(self.generator.parameters(), lr=lr)
+        self.g_optimizer = g_optimizer(self.generator.parameters(), lr=lr, momentum=0.9)
         self.d_optimizer = d_optimizer(self.discriminator.parameters(), lr=lr, momentum=0.9)
 
-        (self.generator, self.discriminator), (self.d_optimizer, self.g_optimizer) = amp.initialize([self.generator, self.discriminator],
+        (self.generator, self.discriminator), (self.g_optimizer, self.d_optimizer) = amp.initialize([self.generator, self.discriminator],
                                                                                                     [self.g_optimizer, self.d_optimizer],
                                                                                                     opt_level="O1",
                                                                                                     num_losses=5)
@@ -55,7 +58,6 @@ class Trainer(nn.Module):
         self.max_iters = 100000
 
         if torch.cuda.is_available():
-            torch.cuda.empty_cache()
             self.cuda()
 
     @property
@@ -70,64 +72,75 @@ class Trainer(nn.Module):
         device = self.device
         return [arg.to(device) for arg in args]
 
-    def train_loop(self, dataloaders, eval_every, generate_every):
+    def train_loop(self, dataloaders, eval_every, generate_every, save_every):
         for batch in tqdm(dataloaders['train']):
             torch.Tensor.add_(self._iter, 1)
             # generator step
-            self.adjust_lr(self.g_optimizer)
-            g_losses = self.g_step(self.adapt(batch))
-            g_stats = self.get_opt_stats(self.g_optimizer, type='generator')
-            self.write_logs(losses=g_losses, stats=g_stats)
+            if self._iter % 2 == 0:
+                self.adjust_lr(self.g_optimizer)
+                g_losses = self.g_step(self.adapt(batch))
+                g_stats = self.get_opt_stats(self.g_optimizer, type='generator')
+                self.write_logs(losses=g_losses, stats=g_stats, type='generator')
 
             #discriminator step
-            self.adjust_lr(self.d_optimizer)
-            d_losses = self.d_step(self.adapt(batch))
-            d_stats = self.get_opt_stats(self.d_optimizer, type='discriminator')
-            self.write_logs(losses=d_losses, stats=d_stats)
+            if self._iter % 2 == 1:
+                self.adjust_lr(self.d_optimizer)
+                d_losses = self.d_step(self.adapt(batch))
+                d_stats = self.get_opt_stats(self.d_optimizer, type='discriminator')
+                self.write_logs(losses=d_losses, stats=d_stats, type='discriminator')
 
-            if (self.iter % eval_every == 0):
+            if self.iter % eval_every == 0:
                 x, labels = batch
                 x = x.cuda()
                 metrics = self.evaluate(x)
                 self.write_logs(metrics=metrics)
-                self.save_checkpoint()
+                metrics = self.evaluate_identification(dataloaders)
+                self.write_logs(metrics=metrics)
 
-            if (self.iter % generate_every == 0):
+
+            if self.iter % generate_every == 0:
                 x, labels = batch
                 x = x[:2, ...].cuda()
                 self.generate(x)
 
+            if self.iter % save_every == 0:
+                self.save_discriminator()
+                self.save_generator()
+
+
     def g_step(self, batch):
         self.g_optimizer.zero_grad()
         mae_loss, bce_loss, ce_loss = self.g_loss(*batch)
-        with amp.scale_loss(mae_loss * self.mae_weight, self.g_optimizer, loss_id=0) as scaled_loss:
+        # with amp.scale_loss(mae_loss * self.generator_mae_weight, self.g_optimizer, loss_id=0) as scaled_loss:
+        #     scaled_loss.backward(retain_graph=True)
+        with amp.scale_loss(bce_loss * self.generator_bce_weight, self.g_optimizer, loss_id=0) as scaled_loss:
             scaled_loss.backward(retain_graph=True)
-        with amp.scale_loss(bce_loss * self.bce_weight, self.g_optimizer, loss_id=1) as scaled_loss:
-            scaled_loss.backward(retain_graph=True)
-        with amp.scale_loss(ce_loss * self.ce_weight, self.g_optimizer, loss_id=2) as scaled_loss:
+        with amp.scale_loss(ce_loss * self.generator_ce_weight, self.g_optimizer, loss_id=1) as scaled_loss:
             scaled_loss.backward()
-
         self.g_optimizer.step()
 
         losses = {
-            'generator_mae': self.mae_weight * mae_loss.item(),
-            'generator_bce': self.bce_weight * bce_loss.item(),
-            'generator_ce':  self.ce_weight * ce_loss.item()
+            # 'mae': self.generator_mae_weight * mae_loss.item(),
+            'bce_fake': self.generator_bce_weight * bce_loss.item(),
+            'ce':  self.generator_ce_weight * ce_loss.item()
         }
         return losses
 
     def d_step(self, batch):
         self.d_optimizer.zero_grad()
-        bce_real, bce_fake = self.d_loss(*batch)
-        with amp.scale_loss(bce_real * self.bce_real_weight, self.d_optimizer, loss_id=3) as scaled_loss:
+        bce_fake, bce_real, ce_loss = self.d_loss(*batch)
+        with amp.scale_loss(bce_fake * self.discriminator_bce_fake_weight, self.d_optimizer, loss_id=2) as scaled_loss:
             scaled_loss.backward(retain_graph=True)
-        with amp.scale_loss(bce_fake * self.bce_fake_weight, self.d_optimizer, loss_id=4) as scaled_loss:
+        with amp.scale_loss(bce_real * self.discriminator_bce_real_weight, self.d_optimizer, loss_id=3) as scaled_loss:
+            scaled_loss.backward(retain_graph=True)
+        with amp.scale_loss(ce_loss * self.discriminator_ce_weight, self.d_optimizer, loss_id=4) as scaled_loss:
             scaled_loss.backward()
         self.d_optimizer.step()
 
         losses = {
-            'discriminator_real': self.bce_real_weight * bce_real.item(),
-            'discriminator_fake': self.bce_fake_weight * bce_fake.item()
+            'bce_real': self.discriminator_bce_real_weight * bce_real.item(),
+            'bce_fake': self.discriminator_bce_fake_weight * bce_fake.item(),
+            'ce': self.discriminator_ce_weight * ce_loss.item()
         }
         return losses
 
@@ -135,8 +148,7 @@ class Trainer(nn.Module):
         src, tgt = torch.chunk(x, 2, dim=0)
         src_label, tgt_label = torch.chunk(labels, 2)
 
-        res, mask = self.generator(src, tgt) # res.shape (bs, 3, 112, 112); mask.shape (bs, 1, 112, 112)
-        fake = mask * res + (1 - mask) * src
+        fake = self.generator(src, tgt)
         mae_loss = self.mae_criterion(fake, src)
 
         logits, fake_realness_logit  = self.discriminator(fake, tgt_label)
@@ -146,30 +158,32 @@ class Trainer(nn.Module):
     
     def d_loss(self, x, labels):
         src, tgt = torch.chunk(x, 2, dim=0)
-        src_label, tgt_label = torch.chunk(labels, 2)
         with torch.no_grad():
-            res, mask = self.generator(src, tgt) # res.shape (bs, 3, 112, 112); mask.shape (bs, 1, 112, 112)
-            fake = mask * res + (1 - mask) * src
-    
-        _, real_realness_logit = self.discriminator(x)
+            fake = self.generator(src, tgt)
+
+        logits, real_realness_logit  = self.discriminator(x, labels)
+        ce_loss = self.ce_criterion(logits, labels)
+
         _, fake_realness_logit = self.discriminator(fake)
 
-        bce_real = self.bce_criterion(real_realness_logit, torch.ones_like(real_realness_logit) - random.uniform(0.1, 0.3)) / 2
         bce_fake = self.bce_criterion(fake_realness_logit, torch.zeros_like(fake_realness_logit) + random.uniform(0.1, 0.3))
-        return bce_real, bce_fake
+        bce_real = self.bce_criterion(real_realness_logit, torch.ones_like(real_realness_logit) - random.uniform(0.1, 0.3)) / 2
+     
+        return bce_fake, bce_real, ce_loss
 
     def evaluate(self, x):
         src, tgt = torch.chunk(x, 2, dim=0)        
 
         self.generator.eval()
-        res, mask = self.generator(src, tgt)
-        fake = mask * res + (1 - mask) * src
+        with torch.no_grad():
+            fake = self.generator(src, tgt)
         self.generator.train()
 
         self.discriminator.eval()
-        src_emb, _  = self.discriminator(src)
-        tgt_emb, _  = self.discriminator(tgt)
-        fake_emb, _ = self.discriminator(fake)
+        with torch.no_grad():
+            src_emb, _  = self.discriminator(src)
+            tgt_emb, _  = self.discriminator(tgt)
+            fake_emb, _ = self.discriminator(fake)
         self.discriminator.train()
 
         src_emb = F.normalize(src_emb)
@@ -196,14 +210,39 @@ class Trainer(nn.Module):
         }
         return metrics
 
+    
+    def evaluate_identification(self, dataloaders):
+        self.discriminator.eval()
+        with torch.no_grad():
+            embs= []
+            for x in tqdm(dataloaders['val']):
+                x = x.cuda()
+                emb, _ = self.discriminator(x)
+                emb = emb.cpu()
+                embs.append(emb)
+            embs = torch.cat(embs, 0)
+            embs = F.normalize(embs)
+
+            y_hat = (embs[0::2,:] * embs[1::2,:]).sum(1).numpy()
+            y_true = np.array(dataloaders['val'].dataset.labels)
+            
+            fpr, tpr, thresholds = metrics.roc_curve(y_true, y_hat)
+            optimal_idx = np.argmax(tpr - fpr)
+            optimal_threshold = thresholds[optimal_idx]
+            acc = metrics.accuracy_score(y_true, y_hat > optimal_threshold)
+        self.discriminator.train()
+        val_metrics = {
+            'identification_acc': acc
+        }
+        return val_metrics
+
 
     def generate(self, x):
         src, tgt = torch.chunk(x, 2, dim=0)        
         x = torch.cat([src, tgt], dim=1)
 
         self.generator.eval()
-        res, mask = self.generator(src, tgt)
-        fake = mask * res + (1 - mask) * src
+        fake = self.generator(src, tgt)
         self.generator.train()
 
         img_tensors = torch.cat([src, fake, tgt], dim=3)
@@ -219,7 +258,7 @@ class Trainer(nn.Module):
         stats = {f'{type}_lr' : optimizer.param_groups[0]['lr']}
         return stats
 
-    def adjust_lr(self, optimizer, warmup=10000):
+    def adjust_lr(self, optimizer, warmup=5000):
         if self.iter <= warmup:
             lr = self.lr * self.iter / warmup 
         else:
@@ -229,10 +268,10 @@ class Trainer(nn.Module):
             group['lr'] = lr
         return lr
 
-    def write_logs(self, losses=None, metrics=None, stats=None):
+    def write_logs(self, losses=None, metrics=None, stats=None, type='loss'):
         if losses:
             for name, value in losses.items():
-                self.writer.add_scalar(f'loss/{name}', value, self.iter)
+                self.writer.add_scalar(f'{type}/{name}', value, self.iter)
         if metrics:
             for name, value in metrics.items():
                 self.writer.add_scalar(f'metric/{name}', value, self.iter)
@@ -240,12 +279,19 @@ class Trainer(nn.Module):
             for name, value in stats.items():
                 self.writer.add_scalar(f'stats/{name}', value, self.iter)
 
-    def save_checkpoint(self, max_checkpoints=100):
+    def save_generator(self, max_checkpoints=100):
         checkpoints = glob.glob(f'{self.model_dir}/*.pt')
         if len(checkpoints) > max_checkpoints:
             os.remove(checkpoints[-1])
-        with open(f'checkpoints/{self.model_dir}/{self.iter}.pt', 'wb') as f:
+        with open(f'checkpoints/{self.model_dir}/generator_{self.iter}.pt', 'wb') as f:
             torch.save(self.generator.state_dict(), f)
+
+    def save_discriminator(self, max_checkpoints=100):
+        checkpoints = glob.glob(f'{self.model_dir}/*.pt')
+        if len(checkpoints) > max_checkpoints:
+            os.remove(checkpoints[-1])
+        with open(f'checkpoints/{self.model_dir}/discriminator_{self.iter}.pt', 'wb') as f:
+            torch.save(self.discriminator.state_dict(), f)
 
     def load_discriminator(self, path, load_last=True):
         if load_last:
