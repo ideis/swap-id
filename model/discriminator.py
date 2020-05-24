b9af8fd51f00a9b9c75c56a11406583dbfe54286
Ilya Kontaev <ilya.kontaev@xperience.ai>
2020-05-24 18:32:41 +0300

    Add new multi-scale discriminator

--- model/discriminator.py
+++ model/discriminator.py
@@ -1,143 +1,69 @@
+import math
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-from math import cos, sin, pi
-
-def repeat(times, fn, *args, **kwargs):
-    return [fn(*args, **kwargs) for _ in range(times)]
-
-class ConvBN(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0, groups=1):
-        super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, groups=groups, bias=False)
-        self.bn = nn.BatchNorm2d(out_channels)
-    
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        return x
-
-
-class ConvBNPReLU(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0, groups=1):
-        super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, groups=groups, bias=False)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.prelu = nn.PReLU()
-    
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.prelu(x)
-        return x
-
-
-class Block(nn.Module):
-    def __init__(self, in_channels, expand_channels, out_channels, residual=False, downsample=False):
-        super().__init__()
-        self.residual = residual
-        stride = 2 if downsample else 1
-
-        self.block = nn.Sequential(
-            ConvBNPReLU(in_channels, expand_channels, kernel_size=1),
-            ConvBNPReLU(expand_channels, expand_channels, kernel_size=3, stride=stride, padding=1, groups=expand_channels),
-            ConvBN(expand_channels, out_channels, kernel_size=1)
-        )
-
-        if self.residual:
-            self.block[-1].bn.weight.data.zero_()        
-
-    def forward(self, x):
-        if self.residual:
-            return self.block(x) + x
-        else:
-            return self.block(x)
-
-
-class ArcMargin(nn.Module):
-    def __init__(self, num_classes, emb_size=512, m=0.4, s=64.):
-        super().__init__()
-        self.weight = nn.Parameter(torch.empty(num_classes, emb_size))
-        nn.init.xavier_normal_(self.weight)
+class ConvNet(nn.Module):
+    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d):
+        super(ConvNet, self).__init__()
+        self.n_layers = n_layers
         
-        self.s = self.scalar(s)
-        self.sin_m, self.cos_m = self.scalar(sin(m)), self.scalar(cos(m))
-        self.th = self.scalar(cos(pi - m))
-        self.mm = self.scalar(sin(pi - m) * m)
-    
-    @staticmethod
-    def scalar(p):
-        return nn.Parameter(torch.tensor(p), requires_grad=False)
+        kernel_size = 4
+        padding = math.ceil((kernel_size - 1.0) / 2)
+        sequence = [[nn.Conv2d(input_nc, ndf, kernel_size=kernel_size, stride=2, padding=padding), nn.LeakyReLU(0.2, True)]]
 
-    def forward(self, x, labels):
-        x = F.normalize(x)
-        W = F.normalize(self.weight)
-        cosine = x @ W.t()
-        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
-        phi = cosine * self.cos_m - sine * self.sin_m
-        phi = torch.where(cosine > self.th, phi, cosine - self.mm)
-        one_hot = torch.zeros_like(cosine)
-        one_hot.scatter_(1, labels.view(-1, 1), 1)
-        output = one_hot * phi + (1. - one_hot) * cosine
-        return output * self.s
+        nf = ndf
+        for n in range(1, n_layers):
+            nf_prev = nf
+            nf = min(nf * 2, 512)
+            sequence += [[
+                nn.Conv2d(nf_prev, nf, kernel_size=kernel_size, stride=2, padding=padding),
+                norm_layer(nf),
+                nn.LeakyReLU(0.2, True)
+            ]]
 
+        nf_prev = nf
+        nf = min(nf * 2, 512)
+        sequence += [[
+            nn.Conv2d(nf_prev, nf, kernel_size=kernel_size, stride=1, padding=padding),
+            norm_layer(nf),
+            nn.LeakyReLU(0.2, True)
+        ]]
 
-class AirMargin(nn.Module):
-    def __init__(self, num_classes, emb_size=512, m=0.4, s=64.):
-        super().__init__()
-        self.weight = nn.Parameter(torch.empty(num_classes, emb_size))
-        nn.init.xavier_normal_(self.weight)
-        self.m = m
-        self.s = s
+        sequence += [[nn.Conv2d(nf, 1, kernel_size=kernel_size, stride=1, padding=padding)]]
 
-    def forward(self, x, label):
-        W = F.normalize(self.weight)
-        x = F.normalize(x)
-        cosine = x @ W.t()
-        theta = torch.acos(cosine)
-        m = torch.zeros_like(theta)
-        m.scatter_(1, label.view(-1, 1), self.m)
-        scale = -2 * self.s / pi
-        return self.s + scale * (theta + m)
+        sequence_stream = []
+        for n in range(len(sequence)):
+            sequence_stream += sequence[n]
+        self.model = nn.Sequential(*sequence_stream)
 
-
-class Discriminator(nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
-        self.backbone = nn.Sequential(
-            ConvBNPReLU(3, 64, kernel_size=3, stride=2, padding=1),
-            ConvBNPReLU(64, 64, kernel_size=1, groups=64),
-            Block(64, 128, 64, downsample=True),
-            *repeat(2, Block, in_channels=64, expand_channels=128, out_channels=64, residual=True),
-            Block(64, 256, 128, downsample=True),
-            *repeat(3, Block, in_channels=128, expand_channels=256, out_channels=128, residual=True),
-            Block(128, 512, 256, downsample=True),
-            *repeat(6, Block, in_channels=256, expand_channels=512, out_channels=256, residual=True),
-            ConvBNPReLU(256, 512, kernel_size=1),
-            ConvBN(512, 512, kernel_size=7, groups=512),
-            nn.Conv2d(512, 512, kernel_size=1),
-            nn.Flatten(1),
-        )
-        self.classifier = nn.Linear(512, 1)
-        self.metric = AirMargin(num_classes)
         self.init_parameters()
-     
-    def forward(self, x, labels=None):
-        features = self.backbone(x)
-        realness_logit = self.classifier(features)
-        if torch.is_tensor(labels):
-            logits = self.metric(features, labels)
-            return logits, realness_logit
-        else:
-            return features, realness_logit
+    def forward(self, input):
+        return self.model(input)
+
 
     def init_parameters(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.ones_(m.weight)
-                nn.init.zeros_(m.bias)
+                nn.init.xavier_normal_(m.weight.data)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, input_nc=3, ndf=64, n_layers=6, n_scales=3, norm_layer=torch.nn.InstanceNorm2d):
+        super(Discriminator, self).__init__()
+        self.n_scales = n_scales
+        self.n_layers = n_layers
+
+        for i in range(n_scales):
+            net = ConvNet(input_nc, ndf, n_layers, norm_layer)
+            setattr(self, 'layer' + str(i), net.model)
+
+        self.downsample = nn.AvgPool2d(3, stride=2, padding=[1, 1], count_include_pad=False)
+
+    def forward(self, x):
+        result = []
+        x_downsampled = x
+        for i in range(self.n_scales):
+            model = getattr(self, 'layer' + str(i))
+            result.append(model(x_downsampled))
+            x_downsampled = self.downsample(x_downsampled)
+        return result
