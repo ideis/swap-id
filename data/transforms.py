ce0ea2629756c614109bbd136cf9d495d8bb03f0
Ilya Kontaev <ilya.kontaev@xperience.ai>
2020-05-24 18:31:45 +0300

    Change transforms

--- data/transforms.py
+++ data/transforms.py
@@ -1,32 +1,21 @@
-import torch
-from torchvision.transforms import *
+import torchvision.transforms as transforms
 
 def transform_train(img):
-    transform = Compose([
-        RandomHorizontalFlip(),
-        ColorJitter(brightness=0.125, contrast=0.125, saturation=0.125),
-        ToTensor(),
-        Normalize(mean=[0.5] * 3, std=[0.5] * 3)
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.ColorJitter(brightness=0.125, contrast=0.125, saturation=0.125),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5] * 3, std=[0.5] * 3)
     ])
 
     img_tensor = transform(img)
     return img_tensor
 
 def transform_val(img):
-    transform = Compose([
-        ToTensor(),
-        Normalize(mean=[0.5] * 3, std=[0.5] * 3)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5] * 3, std=[0.5] * 3)
     ])
 
     img_tensor = transform(img)
     return img_tensor
-
-def transform_inv(img_tensor):
-    transform = Compose([
-        Normalize(mean=[-1.] * 3,
-                  std=[2.] * 3),
-        ToPILImage()
-    ])
-    img = transform(img_tensor)
-    return img
-
