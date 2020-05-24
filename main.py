0552d925b94ad03ba11d5cb4f6af4d1292603597
Ilya Kontaev <ilya.kontaev@xperience.ai>
2020-05-24 18:34:13 +0300

    Update main

--- main.py
+++ main.py
@@ -1,56 +1,49 @@
-import os
 import click
-import numpy as np
 
-import torch
-import torch.nn as nn
 from torch.utils.data import DataLoader
-from apex.optimizers import FusedSGD, FusedAdam
+from torch.optim import Adam
 
-from data.faces import LFW
-from data.faces import Celebs
+from data.faces import FFHQ
 from data.transforms import transform_train, transform_val
-from trainer import Trainer 
+from trainer import Trainer
 
 @click.command()
 @click.option('--model_dir', default='models/default')
-@click.option('--epochs', default=100)
-@click.option('--batch_size', default=256)
-@click.option('--lr', default=0.05)
-@click.option('--momentum', default=0.9)
+@click.option('--epochs', default=20)
+@click.option('--train_batch_size', default=4)
+@click.option('--val_batch_size', default=10)
+@click.option('--lr', default=4e-4)
 @click.option('--warmup', default=1000)
 @click.option('--max_iters', default=100000)
-@click.option('--eval_every', default=200)
+@click.option('--eval_every', default=250)
 @click.option('--generate_every', default=500)
 @click.option('--save_every', default=5000)
-@click.option('--num_workers', default=4)
-def main(model_dir, epochs, batch_size, lr, momentum, warmup, max_iters, eval_every, generate_every, save_every, num_workers):
+@click.option('--num_workers', default=16)
+def main(model_dir, epochs, train_batch_size, val_batch_size, lr, warmup, max_iters, eval_every, generate_every, save_every, num_workers):
 
-    datasets = {'train': Celebs(db_path='datasets/train_db', transform=transform_train),
-                'val': LFW(path='datasets/lfw.bin', transform=transform_val)}
+    datasets = {"train": FFHQ("datasets/ffhq", transform_train, same_person_prob=0.5),
+                "val": FFHQ("datasets/ffhq_val", transform_val, same_person_prob=0)}
 
     dataloaders = {'train': DataLoader(datasets['train'], 
-                                       batch_size,
+                                       batch_size=train_batch_size,
                                        shuffle=True,
                                        num_workers=num_workers,
                                        drop_last=True),
+                   'val': DataLoader(datasets['val'], 
+                                       batch_size=val_batch_size,
+                                       shuffle=False,
+                                       num_workers=num_workers,
+                                       drop_last=True)}
 
-                   'val': DataLoader(datasets['val'],
-                                     batch_size=batch_size,
-                                     shuffle=False,
-                                     num_workers=num_workers)}
-
-    trainer = Trainer(model_dir='mobiface_gan_mae',
-                      g_optimizer=FusedSGD,
-                      d_optimizer=FusedSGD,
+    trainer = Trainer(model_dir='faceshifter',
+                      g_optimizer=Adam,
+                      d_optimizer=Adam,
                       lr=lr,
-                      momentum=momentum,
                       warmup=warmup,
-                      max_iters=max_iters,
-                      num_classes=datasets['train'].num_classes)
+                      max_iters=max_iters)
     
-    trainer.load_discriminator('checkpoints/mobiface_gan_mae', load_last=True)
-    trainer.load_generator('checkpoints/mobiface_gan_mae', load_last=True)
+    trainer.load_discriminator('checkpoints/faceshifter', load_last=True)
+    trainer.load_generator('checkpoints/faceshifter', load_last=True)
 
     for epoch in range(1, epochs + 1):
         trainer.train_loop(dataloaders, eval_every, generate_every, save_every)
